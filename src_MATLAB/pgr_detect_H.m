8416a8af67c8d59552e037e931e1ddd3dd1dc9ff
AlperenAydin <aydin.alperen.bugra@gmail.com>
2016-05-11 16:12:15 +0200

    Fin de Detection d'Harris

--- src_MATLAB/pgr_detect_H.m
+++ src_MATLAB/pgr_detect_H.m
@@ -16,10 +16,20 @@ sigma = 1;
 
 
 
-W =  fspecial('gaussian', 9, 1);
-A = conv2(Ix.^2, W);
-B = conv2(Iy.^2, W);
-C = conv2(Ix.*Iy, W);
+W =  fspecial('gaussian', 10, 2);
+A = imfilter(Ix.^2, W);
+B = imfilter(Iy.^2, W);
+C = imfilter(Ix.*Iy, W);
 
 [h,w] = size(A);
 Harris = zeros(h,w); 
+k = 0.04;
+for x = 1:w
+    for y = 1:h
+        M = [A(y,x), C(y,x) ; C(y,x), B(y,x)];
+        Harris(y,x) = det(M) - k*trace(M);
+    end
+end
+
+imshow(Harris);
+    
