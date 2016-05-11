799cbba7c42ff1a1b5643805733c2a8bb289c382
JeremBlain <blain.jerem@gmail.com>
2016-05-11 17:42:30 +0200

    Add files via upload

--- src_MATLAB/pgr_detect_H.m
+++ src_MATLAB/pgr_detect_H.m
@@ -2,12 +2,13 @@
 
 close all; clear all;
 
-im = rgb2gray( imread('keble_a.jpg' ) );
+image = imread('keble_a.jpg' );
+im = rgb2gray(image);
 
 [Ix, Iy] = gradient(double(im));
 
 
-sigma = 1;
+sigma = 2;
 
 W = fspecial('gaussian', 10, sigma);
 A = conv2(Ix.^2, W, 'same');
@@ -16,4 +17,13 @@ C = conv2(Ix.*Iy,W, 'same');
 k = 0.04;
 
 Harris = A.*B - C.^2 - k*(A+B); 
-imagesc(Harris);
+
+threshold = 5e5;
+[i, j] = find(Harris > threshold);
+
+%affichage de l'image avec les coins
+figure(); hold on;
+imagesc(image), colormap gray, plot(j, i, 'or');
+
+
+
